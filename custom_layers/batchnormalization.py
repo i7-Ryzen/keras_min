97eed77c4ccaa908035c56c7d3032376d057ddd0
soufiane <soufianefadel1@gmail.com>
2020-09-09 18:00:08 -0400

    commit batchnormalization

---
+++ custom_layers/batchnormalization.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+if __name__ == "__main__":
+    import os
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # to ignore warning errors
+    import time
+    import tensorflow.keras.backend as keras
+    import tensorflow.keras.layers as layers
+
+
+    # # # data_type = channels_last
+    # A = np.random.randn(2, 2, 2, 2)
+    # avg_time = [0, 0]
+    # outs = [[], []]
+    # x = keras.constant(A)
+    # batchnormalization_keras = layers.BatchNormalization()
+    #
+    # n_simulations = 10
+    # for _ in range(n_simulations):
+    #     # Keras
+    #     t1 = time.time()
+    #     o1 = batchnormalization_keras(x).numpy()
+    #     avg_time[0] += (time.time() - t1)/n_simulations
+    #     outs[0].append(o1)
+    #
+    #     # our methods
+    #     t1 = time.time()
+    #     o2 = zeropadding2d(A, 1)
+    #     avg_time[1] += (time.time() - t1)/n_simulations
+    #     outs[1].append(o2)
+    #
+    # # print("difference of predictions ", [(o1 - o2).sum() for o1, o2 in zip(*outs)])
+    # # print("the average run time of keras: ", avg_time[0], "the average run time  of our implementation: ", avg_time[1])
+    # # print('Ratio speed: (our_implementation/keras)', avg_time[1] / avg_time[0])
+    #
+    # print(A)
+    # print("#"*10)
+    # print(o1)
+    # # print("#" * 10)
+    # # print(o2)
--- custom_layers/dense.py
+++ custom_layers/dense.py
@@ -1,5 +1,3 @@
-# conda install nomkl  (for mac)
-
 import numpy as np
 
 
@@ -32,7 +30,6 @@ def dense_layer(x, w, b):
   - out: output, of shape (N, M)
   - cache: (x, w, b)
   """
-    out = None
     out = np.dot(x, w) + b
     return out
 
@@ -40,45 +37,46 @@ def dense_layer(x, w, b):
 ######################################################################
 # test : predictions & run_time
 ######################################################################
+
 if __name__ == "__main__":
     import os
-
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # to ignore warning errors
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # to ignore warning errors
     import time
     import tensorflow.keras.backend as keras
     import tensorflow.keras.layers as layers
 
-    A = np.random.randn(3, 200, 200, 3).astype('float32')
-
-    avg_time = [0, 0]
-    outs = [[], []]
-    x = keras.constant(A)
-    dense_keras = layers.Dense(units=1024, activation=None, input_shape=x.shape)
-    y = dense_keras(x).numpy()
-    W, b = dense_keras.get_weights()
-
-    n_simulations = 3
-    for _ in range(n_simulations):
-        # Keras
-        t1 = time.time()
-        o1 = dense_keras(x).numpy()
-        avg_time[0] += (time.time() - t1) / n_simulations
-        outs[0].append(o1)
-
-        # our methods
-        t1 = time.time()
-        o2 = dense_layer(A, W, b)
-        avg_time[1] += (time.time() - t1) / n_simulations
-        outs[1].append(o2)
-
-    print("difference of predictions ", [(o1 - o2).sum() for o1, o2 in zip(*outs)])
-    print("the average run time of keras: ", avg_time[0], "the average run time  of our implementation: ", avg_time[1])
-    print('Ratio speed: (our_implementation/keras)', avg_time[1] / avg_time[0])
-
-#################################################
-# result
-#################################################
-
-# difference of predictions  [9.671998131229864e-05, 9.671998131229864e-05, 9.671998131229864e-05]
-# the average run time of keras:  0.42083819707234704 the average run time  of our implementation:  3.2441244920094805
-# Ratio speed: (our_implementation/keras) 7.708721581305932
+#     A = np.random.randn(3, 200, 200, 3).astype('float32')
+#
+#     avg_time = [0, 0]
+#     outs = [[], []]
+#     x = keras.constant(A)
+#
+#     dense_keras = layers.Dense(units=1024, activation=None, input_shape=x.shape)
+#     y = dense_keras(x).numpy()
+#     W, b = dense_keras.get_weights()
+#
+#     n_simulations = 3
+#     for _ in range(n_simulations):
+#         # Keras
+#         t1 = time.time()
+#         o1 = dense_keras(x).numpy()
+#         avg_time[0] += (time.time() - t1) / n_simulations
+#         outs[0].append(o1)
+#
+#         # our methods
+#         t1 = time.time()
+#         o2 = dense_layer(A, W, b)
+#         avg_time[1] += (time.time() - t1) / n_simulations
+#         outs[1].append(o2)
+#
+#     print("difference of predictions ", [(o1 - o2).sum() for o1, o2 in zip(*outs)])
+#     print("the average run time of keras: ", avg_time[0], "the average run time  of our implementation: ", avg_time[1])
+#     print('Ratio speed: (our_implementation/keras)', avg_time[1] / avg_time[0])
+#
+# #################################################
+# # result
+# #################################################
+#
+# # difference of predictions  [9.671998131229864e-05, 9.671998131229864e-05, 9.671998131229864e-05]
+# # the average run time of keras:  0.42083819707234704 the average run time  of our implementation:  3.2441244920094805
+# # Ratio speed: (our_implementation/keras) 7.708721581305932
